6a82ea981bb4e386f3f772bf64e17cdd1666500f
Nipuna Perera <nipunap@gmail.com>
2020-05-07 22:58:28 +0100

    date added

--- Firmware/RaspberryPi/backend-pi/SensorReader.py
+++ Firmware/RaspberryPi/backend-pi/SensorReader.py
@@ -98,7 +98,7 @@ class SensorReader:
         Temperature in Fahrenheit : %.2f F
         Pressure : %.2f hPa
         Time: %s
-        =======================\n""" % (self.bus_number, self.cTemp, self.fTemp, self.pressure, datetime.now().strftime("%H:%M:%S.%f")))
+        =======================\n""" % (self.bus_number, self.cTemp, self.fTemp, self.pressure, datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")))
 
 
 if __name__ == "__main__" :
