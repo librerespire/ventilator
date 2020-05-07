0188529008894008f879eb5e77df46f1566c1b26
Nipuna Perera <nipunap@gmail.com>
2020-05-07 23:26:13 +0100

    temperature removed from main function and added to different function

--- Firmware/RaspberryPi/backend-pi/SensorReader.py
+++ Firmware/RaspberryPi/backend-pi/SensorReader.py
@@ -17,8 +17,7 @@ class SensorReader:
         self.bus_number = bus_number
         self.bus = smbus.SMBus(self.bus_number)
 
-
-    def sensor_reader(self):
+    def read_temp(self):
         #Reading Data from i2c bus 3
         b1 = self.bus.read_i2c_block_data(0x76, 0x88, 24)
         # Convert the data
@@ -30,6 +29,34 @@ class SensorReader:
         dig_T3 = b1[5] * 256 + b1[4]
         if dig_T3 > 32767 :
             dig_T3 -= 65536
+        # BMP280 address, 0x76(118)
+        # Select Control measurement register, 0xF4(244)
+        #		0x27(39)	Pressure and Temperature Oversampling rate = 1
+        #					Normal mode
+        self.bus.write_byte_data(0x76, 0xF4, 0x27)
+        # BMP280 address, 0x76(118)
+        # Select Configuration register, 0xF5(245)
+        #		0xA0(00)	Stand_by time = 1000 ms
+        self.bus.write_byte_data(0x76, 0xF5, 0xA0)
+        time.sleep(self.delay)
+        # BMP280 address, 0x76(118)
+        # Read data back from 0xF7(247), 8 bytes
+        # Pressure MSB, Pressure LSB, Pressure xLSB, Temperature MSB, Temperature LSB
+        # Temperature xLSB, Humidity MSB, Humidity LSB
+        data = self.bus.read_i2c_block_data(0x76, 0xF7, 8)
+        # Convert temperature data to 19-bits
+        adc_t = ((data[3] * 65536) + (data[4] * 256) + (data[5] & 0xF0)) / 16
+        # Temperature offset calculations
+        var1 = ((adc_t) / 16384.0 - (dig_T1) / 1024.0) * (dig_T2)
+        var2 = (((adc_t) / 131072.0 - (dig_T1) / 8192.0) * ((adc_t)/131072.0 - (dig_T1)/8192.0)) * (dig_T3)
+        t_fine = (var1 + var2)
+        self.cTemp = (var1 + var2) / 5120.0
+        self.fTemp = self.cTemp * 1.8 + 32
+
+    def read_pressure(self):
+        #Reading Data from i2c bus 3
+        b1 = self.bus.read_i2c_block_data(0x76, 0x88, 24)
+        # Convert the data
         # Pressure coefficents
         dig_P1 = b1[7] * 256 + b1[6]
         dig_P2 = b1[9] * 256 + b1[8]
@@ -71,15 +98,8 @@ class SensorReader:
         # Pressure MSB, Pressure LSB, Pressure xLSB, Temperature MSB, Temperature LSB
         # Temperature xLSB, Humidity MSB, Humidity LSB
         data = self.bus.read_i2c_block_data(0x76, 0xF7, 8)
-        # Convert pressure and temperature data to 19-bits
+        # Convert pressure data to 19-bits
         adc_p = ((data[0] * 65536) + (data[1] * 256) + (data[2] & 0xF0)) / 16
-        adc_t = ((data[3] * 65536) + (data[4] * 256) + (data[5] & 0xF0)) / 16
-        # Temperature offset calculations
-        var1 = ((adc_t) / 16384.0 - (dig_T1) / 1024.0) * (dig_T2)
-        var2 = (((adc_t) / 131072.0 - (dig_T1) / 8192.0) * ((adc_t)/131072.0 - (dig_T1)/8192.0)) * (dig_T3)
-        t_fine = (var1 + var2)
-        self.cTemp = (var1 + var2) / 5120.0
-        self.fTemp = self.cTemp * 1.8 + 32
         # Pressure offset calculations
         var1 = (t_fine / 2.0) - 64000.0
         var2 = var1 * var1 * (dig_P6) / 32768.0
@@ -92,19 +112,32 @@ class SensorReader:
         var1 = (dig_P9) * p * p / 2147483648.0
         var2 = p * (dig_P8) / 32768.0
         self.pressure = (p + (var1 + var2 + (dig_P7)) / 16.0) / 100
-        # Output data to screen
-        print("""\t======== bus %d ==========
-        Temperature in Celsius : %.2f C
-        Temperature in Fahrenheit : %.2f F
-        Pressure : %.2f hPa
-        Time: %s
-        =======================\n""" % (self.bus_number, self.cTemp, self.fTemp, self.pressure, datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")))
+        # Output data to screen ** uncomment this part to see values form sensors
+        # print("""\t======== bus %d ==========
+        # Temperature in Celsius : %.2f C
+        # Temperature in Fahrenheit : %.2f F
+        # Pressure : %.2f hPa
+        # Time: %s
+        # =======================\n""" % (self.bus_number, self.cTemp,
+        #             self.fTemp,
+        #             self.pressure,
+        #             datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")))
 
+        def get_pressure():
+            return self.pressure
+
+        def get_ftemp():
+            return self.fTemp
+
+        def get_ctemp():
+            return self.cTemp
 
 if __name__ == "__main__" :
     def threadSlice(index):
         sr = SensorReader(index)
-        sr.sensor_reader()
+        sr.read_pressure()
+        pressure = sr.get_pressure()
+        print("Pressure %.2f" % pressure)
 
     threads = list()
     for index in [1,3,4,5]:
