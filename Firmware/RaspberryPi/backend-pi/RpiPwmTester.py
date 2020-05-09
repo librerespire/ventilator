3d868f744e60af1fe1f7dad17bd4856d172c3464
Chamath D <cham.tutoring@gmail.com>
2020-05-09 16:07:47 +1000

    Added raspberry pi's pwm channels

---
+++ Firmware/RaspberryPi/backend-pi/RpiPwmTester.py
@@ -0,0 +1,35 @@
+import time
+import RPi.GPIO as GPIO
+import logging
+import logging.config
+
+# Constants
+SOL_I = 12
+SOL_E = 13
+PWM_FREQ = 1
+
+# declare logger parameters
+logger = logging.getLogger(__name__)
+logging.config.fileConfig(fname='logger.conf', disable_existing_loggers=False)
+
+
+# Initialize digital output pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(SOL_I, GPIO.OUT)
+GPIO.setup(SOL_E, GPIO.OUT)
+
+pwm_i = GPIO.PWM(SOL_I, PWM_FREQ)
+pwm_e = GPIO.PWM(SOL_E, PWM_FREQ)
+
+pwm_i.start(0)
+pwm_e.start(0)
+
+pwm_i.ChangeDutyCycle(0.5)
+pwm_e.ChangeDutyCycle(0.5)
+
+# while True:
+#     pwm_i.ChangeDutyCycle(0.8)
+#     pwm_e.ChangeDutyCycle(0.8)
+#     time.sleep(2)
