4986d1018a1f2c9124b00469e3167186b41947cc
Nipuna Perera <nipunap@gmail.com>
2020-05-11 16:45:19 +0100

    test case changed

--- Firmware/RaspberryPi/backend-pi/RnD/mqttPublisherTest.py
+++ Firmware/RaspberryPi/backend-pi/RnD/mqttPublisherTest.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 import paho.mqtt.client as mqtt
+import random
 
 # This is the Publisher
 
 client = mqtt.Client()
 client.connect("localhost", 1883, 60)
-client.publish("Ventilator/p1", 10);
-client.publish("Ventilator/p2", 20);
+client.publish("Ventilator/pressure", random.uniform(0,20));
+client.disconnect();
+
+client.connect("localhost", 1883, 60)
+client.publish("Ventilator/flow_rate", random.uniform(0,20));
 client.disconnect();
