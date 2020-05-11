3229269e446c17103f6480b1d859990c81f8b850
Nipuna Perera <nipunap@gmail.com>
2020-05-11 20:25:31 +0100

    loggin added to MQTT class

--- Firmware/RaspberryPi/backend-pi/MQTTTransceiver.py
+++ Firmware/RaspberryPi/backend-pi/MQTTTransceiver.py
@@ -1,7 +1,9 @@
 import paho.mqtt.client as mqtt
 import threading
+import logging
+import logging.config
 
-
+logger = logging.getLogger(__name__)
 class MQTTTransceiver:
     PRESSURE_TOPIC = 'Ventilator/pressure'
     FLOWRATE_TOPIC = 'Ventilator/flow_rate'
@@ -15,11 +17,12 @@ class MQTTTransceiver:
     def mqtt_publish(topic, value):
         client = mqtt.Client()
         client.connect("127.0.0.1", 1883, 60)
+        logger.DEBUG("MQTT Send: [%s] - [%s]" % (topic, message))
         client.publish(topic, value)
         client.disconnect()
 
     def sender(self, topic, value):
-        thread = threading.Thread(target=mqtt_publish, args=(topic, value,))
+        thread = threading.Thread(target=self.mqtt_publish, args=(topic, value,))
         thread.start()
 
     def mqtt_subscriber(self):
