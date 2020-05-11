b92bd9d6df68a9dd208f03541d05ccd1489de661
Nipuna Perera <nipunap@gmail.com>
2020-05-11 22:26:38 +0100

    array sending as json

--- Firmware/RaspberryPi/frontend-pi/routes/graphs.js
+++ Firmware/RaspberryPi/frontend-pi/routes/graphs.js
@@ -4,15 +4,15 @@ var database = require("../utils/database.js")
 var router = express.Router();
 
 router.get('/pressure', function (req, res) {
-  res.json(JSON.stringify(database.get_pressure()));
+  res.json(database.get_pressure());
 })
 
 router.get('/flowrate', function (req, res) {
-  res.json(JSON.stringify(database.get_flow_rate()));
+  res.json(database.get_flow_rate());
 })
 
 router.get('/volume', function (req, res) {
-  res.json(JSON.stringify(database.get_volume()));
+  res.json(database.get_volume());
 })
 
 module.exports = router;
