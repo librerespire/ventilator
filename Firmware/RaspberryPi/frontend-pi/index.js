13f92c9c44ade8fcce7959d17604b8c1d4bb9c68
Nipuna Perera <nipunap@gmail.com>
2020-05-11 21:16:18 +0100

    removed / route

--- Firmware/RaspberryPi/frontend-pi/index.js
+++ Firmware/RaspberryPi/frontend-pi/index.js
@@ -11,10 +11,6 @@ app.use(express.static(__dirname + '/public'));
 app.use(logger('combined'));
 app.use('/api/graphs', graphs);
 
-app.get("/", (req, res) => {
-  res.status(200).send("move to /index.html");
-});
-
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
 });
