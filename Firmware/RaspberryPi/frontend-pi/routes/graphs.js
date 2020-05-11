c8063ce3e68cfa149d0e1a56af4d0f76cc38fdde
Nipuna Perera <nipunap@gmail.com>
2020-05-11 22:24:53 +0100

    json response added

--- Firmware/RaspberryPi/frontend-pi/routes/graphs.js
+++ Firmware/RaspberryPi/frontend-pi/routes/graphs.js
@@ -4,15 +4,15 @@ var database = require("../utils/database.js")
 var router = express.Router();
 
 router.get('/pressure', function (req, res) {
-  res.send(JSON.stringify(database.get_pressure()));
+  res.json(JSON.stringify(database.get_pressure()));
 })
 
 router.get('/flowrate', function (req, res) {
-  res.send(JSON.stringify(database.get_flow_rate()));
+  res.json(JSON.stringify(database.get_flow_rate()));
 })
 
 router.get('/volume', function (req, res) {
-  res.send(JSON.stringify(database.get_volume()));
+  res.json(JSON.stringify(database.get_volume()));
 })
 
 module.exports = router;
