ff93e473f7856cac72b8018588c5523c7a494822
Nipuna Perera <nipunap@gmail.com>
2020-05-11 20:50:04 +0100

    UI integration

--- Firmware/RaspberryPi/frontend-pi/routes/graphs.js
+++ Firmware/RaspberryPi/frontend-pi/routes/graphs.js
@@ -11,4 +11,8 @@ router.get('/flowrate', function (req, res) {
   res.send(JSON.stringify(database.get_flow_rate()));
 })
 
+router.get('/volume', function (req, res) {
+  res.send(JSON.stringify(database.get_volume()));
+})
+
 module.exports = router;
--- Firmware/RaspberryPi/frontend-pi/utils/database.js
+++ Firmware/RaspberryPi/frontend-pi/utils/database.js
@@ -6,36 +6,30 @@ module.exports = {
   set_pressure: function(data){
     pressure.pop()
     pressure.unshift(data)
-    console.log(pressure);
     return pressure
   },
 
   set_flow_rate: function(data){
     flow_rate.pop()
     flow_rate.unshift(data)
-    console.log(flow_rate);
     return flow_rate
   },
 
   set_volume: function(data){
     volume.pop()
     volume.unshift(data)
-    console.log(volume);
     return volume
   },
 
   get_volume: function(){
-    console.log(volume);
     return volume
   },
 
   get_pressure: function(){
-    console.log(pressure);
     return pressure
   },
 
-  get_flow_rate: function(){
-    console.log(flow_rate);
+  get_flow_rate: function(){    
     return flow_rate
   }
 }
