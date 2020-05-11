c8c2209936eddfe9ee2a125b4495a4dd5f2dbe87
Nipuna Perera <nipunap@gmail.com>
2020-05-11 16:31:06 +0100

    add more logs to debug database.js

--- Firmware/RaspberryPi/frontend-pi/utils/database.js
+++ Firmware/RaspberryPi/frontend-pi/utils/database.js
@@ -5,20 +5,24 @@ module.exports = {
   add_pressure: function(data){
     pressure.unshift(data)
     pressure.pop()
+    console.log(pressure);
     return pressure
   },
 
   add_flow_rate: function(data){
     flow_rate.unshift(data)
     flow_rate.pop()
+    console.log(flow_rate);
     return flow_rate
   },
 
   get_pressure: function(){
+    console.log(pressure);
     return pressure
   },
 
   get_flow_rate: function(){
+    console.log(flow_rate);
     return flow_rate
   }
 }
