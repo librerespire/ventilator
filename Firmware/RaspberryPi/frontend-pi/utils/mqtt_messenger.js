e50513488d25089ce117a4e9015966f4b069aa2e
Nipuna Perera <nipunap@gmail.com>
2020-05-11 16:39:43 +0100

    errors fixed in mqtt module

--- Firmware/RaspberryPi/frontend-pi/utils/mqtt_messenger.js
+++ Firmware/RaspberryPi/frontend-pi/utils/mqtt_messenger.js
@@ -2,7 +2,7 @@ const mqtt = require('mqtt')
 var database = require("./database.js")
 const client = mqtt.connect('mqtt://localhost')
 
-module.exports = {
+var self = module.exports = {
   mqtt_sender: function() {
     console.log("Sender called");
   },
@@ -17,24 +17,24 @@ module.exports = {
     client.on('message', (topic, message) => {
       switch (topic) {
         case 'Ventilator/pressure':
-          return module.exports.mqtt_pressure()
+          return self.mqtt_pressure(message)
         case 'Ventilator/flow_rate':
-          return database.flow_rate(message)
+          return self.mqtt_flowrate(message)
       }
       console.log('No handler for topic %s', topic)
     });
   },
 
-  mqtt_pressure: function(message){
+  mqtt_pressure: function(message) {
     console.log(message)
     database.add_pressure(message)
-    console.log(database.get_pressure)
+    console.log(database.get_pressure())
   },
 
-  mqtt_flowrate: function(message){
+  mqtt_flowrate: function(message) {
     console.log(message)
     database.add_flow_rate(message)
-    console.log(database.get_flow_rate)
+    console.log(database.get_flow_rate())
   }
 
 };
