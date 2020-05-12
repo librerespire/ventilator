8e164965b7e37928edef4eae59d0f415ecccaf93
Nipuna Perera <nipunap@gmail.com>
2020-05-12 14:38:11 +0100

    put method changed to get

--- Firmware/RaspberryPi/frontend-pi/routes/configs.js
+++ Firmware/RaspberryPi/frontend-pi/routes/configs.js
@@ -9,27 +9,27 @@ const PEEP_CONFIG_TOPIC = 'Config/peep';
 const VT_CONFIG_TOPIC = 'Config/vt';
 const IE_CONFIG_TOPIC = 'Config/ie';
 
-router.put('/fio2', function (req, res) {
+router.get('/fio2', function (req, res) {
   mqtt_messenger.mqtt_sender(FIO2_CONFIG_TOPIC, req.query.fio2);
   res.sendStatus(200);
 })
 
-router.put('/rr', function (req, res) {
+router.get('/rr', function (req, res) {
   mqtt_messenger.mqtt_sender(RR_CONFIG_TOPIC, req.query.rr);
   res.sendStatus(200);
 })
 
-router.put('/vt', function (req, res) {
+router.get('/vt', function (req, res) {
   mqtt_messenger.mqtt_sender(VT_CONFIG_TOPIC, req.query.vt);
   res.sendStatus(200);
 })
 
-router.put('/peep', function (req, res) {
+router.get('/peep', function (req, res) {
   mqtt_messenger.mqtt_sender(PEEP_CONFIG_TOPIC, req.query.peep);
   res.sendStatus(200);
 })
 
-router.put('/ie', function (req, res) {
+router.get('/ie', function (req, res) {
   mqtt_messenger.mqtt_sender(IE_CONFIG_TOPIC, req.query.ie);
   res.sendStatus(200);
 })
