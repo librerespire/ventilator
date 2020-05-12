1c406b50aba9f421d9ab7f3144b749d8b4e48500
Nipuna Perera <nipunap@gmail.com>
2020-05-12 18:49:42 +0100

    debug logs added

--- Firmware/RaspberryPi/backend-pi/MQTTTransceiver.py
+++ Firmware/RaspberryPi/backend-pi/MQTTTransceiver.py
@@ -48,7 +48,7 @@ class MQTTTransceiver:
         client.loop_forever()
 
     def on_message(self, client, obj, msg):
-        print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+        print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload.decode()))
         if (msg.topic == self.FIO2_CONFIG_TOPIC):
             self.va.set_fio2(float(msg.payload.decode()))
             logger.debug("FIO2 receved: %.2f" % self.va.fio2)
--- Firmware/RaspberryPi/backend-pi/Variables.py
+++ Firmware/RaspberryPi/backend-pi/Variables.py
@@ -1,3 +1,7 @@
+import logging
+import logging.config
+
+logger = logging.getLogger(__name__)
 
 class Variables:
     fio2 = 0
@@ -7,6 +11,7 @@ class Variables:
     peep = 0
 
     def set_fio2(self, val):
+        logger.debug("Fio2 set %.2f" % val)
         self.fio2 = val
 
     def set_rr(self, val):
