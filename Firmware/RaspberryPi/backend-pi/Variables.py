85f99bb4f4ee1077f56cbe7557b4f8aa3b31e6c2
Chamath D <cham.tutoring@gmail.com>
2020-05-14 00:30:38 +1000

    Changed default input parameters

--- Firmware/RaspberryPi/backend-pi/Variables.py
+++ Firmware/RaspberryPi/backend-pi/Variables.py
@@ -6,8 +6,8 @@ logger = logging.getLogger(__name__)
 
 class Variables:
     fio2 = 0
-    rr = 12
+    rr = 10
     ie_i = 1  # I part of IE ratio
-    ie_e = 2  # E part of IE ratio
+    ie_e = 1  # E part of IE ratio
     vt = 5
     peep = 1200
