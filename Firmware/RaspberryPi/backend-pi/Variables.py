83e50b3b4fef9b4479084abbdb8fe84006287bfe
Chamath D <cham.tutoring@gmail.com>
2020-05-13 19:41:36 +1000

    Initialized input variables with default values

--- Firmware/RaspberryPi/backend-pi/Variables.py
+++ Firmware/RaspberryPi/backend-pi/Variables.py
@@ -3,10 +3,11 @@ import logging.config
 
 logger = logging.getLogger(__name__)
 
+
 class Variables:
     fio2 = 0
-    rr = 0
-    ie_i = 1    # I part of IE ratio
-    ie_e = 0    # E part of IE ratio
-    vt = 0
-    peep = 0
+    rr = 12
+    ie_i = 1  # I part of IE ratio
+    ie_e = 2  # E part of IE ratio
+    vt = 5
+    peep = 1200
