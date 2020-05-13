0e62544c10afaa2bfa2fdccb0e46c1c5a309cce4
Chamath D <cham.tutoring@gmail.com>
2020-05-14 00:04:28 +1000

    Added debug logging for control_solonoid

--- Firmware/RaspberryPi/backend-pi/Controller.py
+++ Firmware/RaspberryPi/backend-pi/Controller.py
@@ -135,6 +135,7 @@ def control_solenoid(pin, duty_ratio):
     if pin == SI_PIN:
         PWM_I.ChangeDutyCycle(duty_ratio)
     elif pin == SE_PIN:
+        logger.debug("EXP duty_ratio = " + str(DUTY_RATIO_100 - duty_ratio))
         # Expiratory solenoid is normally OPEN. Hence flipping the duty ratio
         PWM_E.ChangeDutyCycle(DUTY_RATIO_100 - duty_ratio)
 
