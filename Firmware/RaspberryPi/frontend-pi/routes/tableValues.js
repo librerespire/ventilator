dd2946524421c0ec0adc83547a8056a50ad1f1f2
Nipuna Perera <nipunap@gmail.com>
2020-05-15 16:06:19 +0100

    minuite volume and pressure support added

--- Firmware/RaspberryPi/frontend-pi/routes/tableValues.js
+++ Firmware/RaspberryPi/frontend-pi/routes/tableValues.js
@@ -3,9 +3,19 @@ var database = require("../utils/database.js")
 
 var router = express.Router();
 
-router.get('/values', function (req, res) {
-  res.json({"vt": database.get_vt()});
-  console.log(database.get_vt());
+router.get('/values', function(req, res) {
+
+  //get Minute Volume by getting sum of last minute delivered volume
+  volumeData = database.get_volume();
+  var min_volume = volumeData.reduce(function(a, b) {
+    return a + b;
+  }, 0);
+
+  res.json({
+    "vt": database.get_vt(),
+    "peak_pressure": Math.max(database.get_pressure()),
+    "minute_volume": min_volume,
+  });
 })
 
 module.exports = router;
