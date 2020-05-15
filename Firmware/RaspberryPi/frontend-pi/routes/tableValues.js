c3f9d8a5ec4d317ec89c5137efb878853214cea1
Nipuna Perera <nipunap@gmail.com>
2020-05-15 16:13:26 +0100

    min error fixed

--- Firmware/RaspberryPi/frontend-pi/routes/tableValues.js
+++ Firmware/RaspberryPi/frontend-pi/routes/tableValues.js
@@ -6,15 +6,15 @@ var router = express.Router();
 router.get('/values', function(req, res) {
 
   //get Minute Volume by getting sum of last minute delivered volume
-  volumeData = database.get_volume();
-  var min_volume = volumeData[1].reduce(function(a, b) {
+  volumeData = database.get_volume()[1];
+  var min_volume = volumeData.reduce(function(a, b) {
     return a + b;
   }, 0);
 
   res.json({
     "vt": database.get_vt(),
     "peak_pressure": Math.max(database.get_pressure()),
-    "minute_volume": min_volume[1],
+    "minute_volume": min_volume,
   });
 })
 
