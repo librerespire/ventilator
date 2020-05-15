fb05dfab28d701b483c1e591ab57ed9fbbcd014d
Nipuna Perera <nipunap@gmail.com>
2020-05-15 16:42:21 +0100

    miising ; added

--- Firmware/RaspberryPi/frontend-pi/routes/tableValues.js
+++ Firmware/RaspberryPi/frontend-pi/routes/tableValues.js
@@ -8,11 +8,11 @@ router.get('/values', function(req, res) {
   //get Minute Volume by getting sum of last minute delivered volume
   volumeData = database.get_volume();
   pressure = database.get_pressure();
-  min_volume = 0
+  min_volume = 0;
   for(var i = 0; i <= volumeData.length; i++) {
           min_volume = min_volume + volumeData[i][1];
   }
-  max_pressure = 0
+  max_pressure = 0;
   for(var i = 0; i <= pressure.length; i++) {
           if (max_pressure < pressure[i][1]){
             max_pressure = pressure[i][1];
