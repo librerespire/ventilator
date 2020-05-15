991c4a76e2c41ff3d9de6b72accd6624eca817e5
Nipuna Perera <nipunap@gmail.com>
2020-05-15 11:44:10 +0100

    x axis changed to datetime

--- Firmware/RaspberryPi/frontend-pi/utils/database.js
+++ Firmware/RaspberryPi/frontend-pi/utils/database.js
@@ -1,42 +1,48 @@
-var pressure = new Array(60).fill(0).map(() => new Array(2).fill(Date.parse('0000-00-00 00:00:00')))
-var flow_rate = new Array(60).fill(0).map(() => new Array(2).fill(Date.parse('0000-00-00 00:00:00')));
-var volume = new Array(60).fill(0).map(() => new Array(2).fill(Date.parse('0000-00-00 00:00:00')))
+var pressure = [];
+var flow_rate = [];
+var volume = [];
 
 
 module.exports = {
-  set_time: function(data){
+  set_time: function(data) {
     time.pop()
     time.unshift(data)
     return time
   },
 
-  set_pressure: function(data){
-    pressure.pop()
+  set_pressure: function(data) {
+    if (pressure.length >= 60) {
+      pressure.pop()
+    }
     pressure.unshift(data)
     return pressure
   },
 
-  set_flow_rate: function(data){
-    flow_rate.pop()
+  set_flow_rate: function(data) {
+    if (flow_rate.length >= 60) {
+      flow_rate.pop()
+    }
     flow_rate.unshift(data)
     return flow_rate
   },
 
-  set_volume: function(data){
-    volume.pop()
+  set_volume: function(data) {
+    if (volume.length >= 60) {
+      volume.pop()
+    }
     volume.unshift(data)
     return volume
   },
 
-  get_volume: function(){
+  get_volume: function() {
     return volume
   },
 
-  get_pressure: function(){
+  get_pressure: function() {
     return pressure
   },
 
-  get_flow_rate: function(){
+  get_flow_rate: function() {
     return flow_rate
   }
 }
--- Firmware/RaspberryPi/frontend-pi/utils/mqtt_messenger.js
+++ Firmware/RaspberryPi/frontend-pi/utils/mqtt_messenger.js
@@ -61,7 +61,6 @@ var self = module.exports = {
     json_data = JSON.parse(message)
 
     time = (new Date(json_data.time)).getTime()
-    console.log(time);
     pressure = Number((json_data.pressure).toFixed(2))
     flow_rate = Number((json_data.flow_rate).toFixed(2))
     volume = Number((json_data.volume).toFixed(2))
