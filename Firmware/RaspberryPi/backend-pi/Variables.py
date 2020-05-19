116a8dfd0e327a5d0cbf32afb450c0a3f44616cb
Chamath D <cham.tutoring@gmail.com>
2020-05-19 17:15:39 +1000

    Calibration kicks in only after user entered flow rate via GUI

--- Firmware/RaspberryPi/backend-pi/Variables.py
+++ Firmware/RaspberryPi/backend-pi/Variables.py
@@ -9,7 +9,7 @@ class Variables:
     BUS_2 = 3 # second inspiratory pressure sensor
     BUS_3 = 4 # first expiratory pressure sensor
     BUS_4 = 5 # second expiratory pressure sensor
-    calib_flow_rate = 10    # flow rate for calibration. TODO: make it -1, once front-end calibration step is ready
+    calib_flow_rate = -1    # flow rate used for calibration. Set by GUI
     fio2 = 0    # fio2 value
     ie = 1      # I:E ratio
     rr = 10     # respiratory rate (RR)
