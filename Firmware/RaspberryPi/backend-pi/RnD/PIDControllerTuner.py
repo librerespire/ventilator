61e5f16f30fbad969cf25212c453f3124d504a65
Chamath D <cham.tutoring@gmail.com>
2020-05-19 20:19:51 +1000

    Setup tuner class to control P3 pressure value

--- Firmware/RaspberryPi/backend-pi/RnD/PIDControllerTuner.py
+++ Firmware/RaspberryPi/backend-pi/RnD/PIDControllerTuner.py
@@ -1,37 +1,58 @@
 from PID import PID
+from SensorReaderService import SensorReaderService
+from Variables import Variables
 import time
-import os.path
+import RPi.GPIO as GPIO
 
-targetT = 10
+# Constants
+PWM_FREQ = 2  # frequency for PWM
+SI_PIN = 12  # PIN (PWM) for inspiratory solenoid
+PWM_I = None
+
+Psupport = 10
 P = 10
 I = 1
 D = 1
 sampling_time = 0.5
 
 pid = PID(P, I, D)
-pid.SetPoint = targetT
+pid.SetPoint = Psupport
 pid.setSampleTime(sampling_time)
 
-count = 0
-pressureArray = [0, 1, 4, 7, 9, 7, 6, 5, 7, 8, 10, 10]
+# count = 0
+# pressureArray = [0, 1, 4, 7, 9, 7, 6, 5, 7, 8, 10, 10]
+#
+#
+# def readPressure():
+#     return pressureArray[count]
 
 
-def readPressure():
-    return pressureArray[count]
+def init_parameters():
+    global PWM_I
+
+    # Initialize PWM pins
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    GPIO.setup(SI_PIN, GPIO.OUT)
+
+    PWM_I = GPIO.PWM(SI_PIN, PWM_FREQ)
+
+    # Start the sensor reading service
+    sensing_service = SensorReaderService()
 
 
-while count < 12:
+while True:
     # read pressure data
-    pressure = readPressure()
+    pressure = Variables.p3
 
     pid.update(pressure)
-    targetPwm = pid.output
-    targetPwm = max(min(int(targetPwm), 100), 0)
+    target_duty_ratio = pid.output
+    target_duty_ratio = max(min(int(target_duty_ratio), 100), 0)
 
-    print("Target: %.1f | Current: %.1f | PWM: %s %%" % (targetT, pressure, targetPwm))
+    print("Target: %.1f | Current: %.1f | Duty Ratio: %s %%" % (Psupport, pressure, target_duty_ratio))
 
-    # Set PWM expansion channel 0 to the target setting
-    # pwmExp.setupDriver(0, targetPwm, 0)
+    # Set PWM to target duty
+    PWM_I.ChangeDutyCycle(target_duty_ratio)
 
-    time.sleep(sampling_time-0.005)
-    count += 1
+    time.sleep(sampling_time - 0.005)
