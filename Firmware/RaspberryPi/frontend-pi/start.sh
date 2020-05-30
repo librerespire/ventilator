8f6ef7bf8ebe7e20d5f082342402e155e4619c16
Nipuna Perera <nipunap@gmail.com>
2020-05-30 13:13:36 +0100

    UI visual changes

--- Firmware/RaspberryPi/frontend-pi/start.sh
+++ Firmware/RaspberryPi/frontend-pi/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 nohup node index.js > /dev/null &
-DISPLAY=:0 chromium-browser --app="http://localhost:8000" --start-fullscreen --force-device-scale-factor=1.50 > /dev/null 2> /dev/null &
+DISPLAY=:0 chromium-browser --app="http://localhost:8000" --start-fullscreen --force-device-scale-factor=1.0 > /dev/null 2> /dev/null &
 echo "$(date) - Frontend started!!"
 exit 0
