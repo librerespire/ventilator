0903066067d1692bd7f33d9674656dc247f9522c
Nipuna Perera <nipunap@gmail.com>
2020-05-30 12:16:11 +0100

    images added and start.sh changed

--- Firmware/RaspberryPi/frontend-pi/start.sh
+++ Firmware/RaspberryPi/frontend-pi/start.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 nohup node index.js > /dev/null &
+DISPLAY=:0 chromium-browser --app="http://localhost:8000" --start-fullscreen > /dev/null &
+echo "$(date) - Frontend started!!"
+exit 0
