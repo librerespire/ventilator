12be907a8a065d717ecd73d57579557d28a366c5
Chamath D <cham.tutoring@gmail.com>
2020-06-27 09:29:33 +1000

    Added extra logs to AlarmTester

--- Firmware/RaspberryPi/backend-pi/RnD/AlarmTester.py
+++ Firmware/RaspberryPi/backend-pi/RnD/AlarmTester.py
@@ -9,15 +9,19 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+print("going to mqtt")
 mqtt = MQTTTransceiver()
+print("going to AlarmManager")
 alarms = AlarmManager(mqtt)
 
 pmax = 15
 p=20
+print("going to raise an alarm")
 alarms.raise_alarm(AlarmType.PMAX_REACHED, AlarmLevel.MINOR, "Current pressure (%.1f) has exceeded Pmax (%.0f)" % (p,pmax))
 
 time.sleep(5)
 p = 14.5
+print("going to raise an alarm")
 alarms.clear_alarm(AlarmType.PMAX_REACHED, "Current pressure (%.1f) has dropped below pmax (%.0f)" % (p, pmax))
 
 logger.debug("Exiting")
