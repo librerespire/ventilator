a241abeb7526a6302d2fe364160e054e47732891
Nipuna Perera <nipunap@gmail.com>
2020-06-27 01:14:15 +0100

    logs added to frontend

--- Firmware/RaspberryPi/frontend-pi/routes/alarms.js
+++ Firmware/RaspberryPi/frontend-pi/routes/alarms.js
@@ -5,11 +5,13 @@ var router = express.Router();
 
 router.get('/major', function(req, res) {
   major_alarms = database.get_major_alarm();
+  console.log(major_alarms);
   res.json(major_alarms);
 })
 
 router.get('/minor', function(req, res) {
   minor_alarm = database.get_minor_alarm();
+  console.log(major_alarms);
   res.json(minor_alarm);
 })
 
