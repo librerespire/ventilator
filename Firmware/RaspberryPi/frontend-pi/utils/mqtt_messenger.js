343f909f2f39fa53501b16a985cae8e8f88c73d1
Chamath D <cham.tutoring@gmail.com>
2020-06-27 15:43:42 +1000

    Added seconds to the alarm display text

--- Firmware/RaspberryPi/frontend-pi/utils/mqtt_messenger.js
+++ Firmware/RaspberryPi/frontend-pi/utils/mqtt_messenger.js
@@ -61,14 +61,15 @@ var self = module.exports = {
   },
 
   format_date: function (date) {
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
+    var hours = date.getHours()
+    var minutes = date.getMinutes()
+    var seconds = date.getSeconds()
+    var ampm = hours >= 12 ? 'pm' : 'am'
+    hours = hours % 12
     hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0'+minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
-    return strTime;
+    minutes = minutes < 10 ? '0'+minutes : minutes
+    var strTime = hours + ':' + minutes + ':' + seconds +' ' + ampm
+    return strTime
 },
 
   mqtt_chartdata: function(message) {
