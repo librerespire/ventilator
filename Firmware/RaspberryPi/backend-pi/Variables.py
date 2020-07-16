ca4cdc1909f991131a06543b5119729a5ee5f4ad
Chamath D <cham.tutoring@gmail.com>
2020-07-16 21:40:39 +1000

    Updated PID parameters based on experimental results

--- Firmware/RaspberryPi/backend-pi/Variables.py
+++ Firmware/RaspberryPi/backend-pi/Variables.py
@@ -43,7 +43,7 @@ class Variables:
     Kp = 5
     Ki = 0.5
     Kd = 0.75
-    pid_sampling_period = 0.25
+    pid_sampling_period = 0.215
 
     # Flag to indicate demo setup, where P3 is read from a BME680 sensor
     demo = True
